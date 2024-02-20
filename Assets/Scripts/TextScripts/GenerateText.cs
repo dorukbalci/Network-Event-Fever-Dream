933a178714ddcb85e3a99eb3bf38250968064a4b
Doruk Balci <c.d.balci@gmail.com>
2024-02-20 17:10:59 +0000

    text generator etc

--- Assets/Scripts/TextScripts/GenerateText.cs
+++ Assets/Scripts/TextScripts/GenerateText.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityTracery;
 
@@ -11,33 +13,50 @@ public class GenerateText : MonoBehaviour
     public TextAsset playerNeg;
 
     public TraceryGrammar Grammar;
-    public TextMeshProUGUI bmanText;
-    public TextMeshProUGUI playerPosText;
-    public TextMeshProUGUI playerNegText;
+    public TextMeshProUGUI bmanBox;
+    public TextMeshProUGUI playerPosBox;
+    public TextMeshProUGUI playerNegBox;
+    
+    //typewriter effect vars
+    public float delay = 0.1f;
+    private string fullText;
+    private string currentText = "";
     
     // Start is called before the first frame update
     void OnEnable()
     {
         UpdateGrammar(businessmanIntro.text);
-        GenerateOutput(bmanText);
+        GenerateOutput(bmanBox);
         
         UpdateGrammar(playerPos.text);
-        GenerateOutput(playerPosText);
+        GenerateOutput(playerPosBox);
         
         UpdateGrammar(playerNeg.text);
-        GenerateOutput(playerNegText);
+        GenerateOutput(playerNegBox);
     }
 
     public void UpdateGrammar(string grammartext) {
         Grammar = new TraceryGrammar(grammartext);
     }
     
-    public void GenerateOutput(TextMeshProUGUI textOutput)
+    public void GenerateOutput(TextMeshProUGUI textBlock)
     {
         var output = Grammar.Generate();
-        textOutput.text = output;
+        fullText = output;
+        StartCoroutine(ShowText(textBlock));
+        //textBlock.text = output;
     }
-    
-    
+
+    IEnumerator ShowText(TextMeshProUGUI textBlock)
+    {
+        for (int i = 0; i < fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            textBlock.text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+  
     
 }
