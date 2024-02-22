b28a8a5ec0958d8b51c9c6eec7a2ab47df5d0600
Doruk Balci <c.d.balci@gmail.com>
2024-02-22 18:59:09 +0000

    last edits

--- Assets/ChangeScene.cs
+++ Assets/ChangeScene.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,10 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
     }
 
     public void buttonPressed()
